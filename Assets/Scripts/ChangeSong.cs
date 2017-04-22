f59fa20eac57cbd9d11d8e5948c551becf60a5ce
Evan Gibbs <evanegibbs@gmail.com>
2017-04-21 20:00:41 -0700

    Finished Music Player

---
+++ Assets/Scripts/ChangeSong.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSong : MonoBehaviour {
+
+	public int songNumber;
+	public float fadeTime;
+	public bool stopMusic;
+
+	private MusicPlayer musicPlayer;
+
+	// Use this for initialization
+	void Start () {
+		musicPlayer = GameObject.FindObjectOfType<MusicPlayer>();
+		if (stopMusic) {
+			musicPlayer.StopMusic(fadeTime);
+		}
+		else {
+			musicPlayer.ChangeMusic(songNumber, fadeTime);
+		}
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public AudioClip[] musicList;
+
+	static MusicPlayer instance = null;
+
+	private AudioSource audioSource;
+	private bool startFadeIn = false;
+	private float fadingTime = 0;
+	private float fadeInTime = 0;
+	private int newSongNumber = 0;
+	private bool stopMusic = false;
+	private float stopFade = 0;
+
+	void Awake (){
+		if (instance != null){
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void Update() {
+		if (fadingTime > 0) {
+			audioSource.volume -= Time.deltaTime / fadingTime;
+			if (audioSource.volume <= 0) {
+				fadingTime = 0;
+				audioSource.clip = musicList[newSongNumber];
+				audioSource.Play();
+				audioSource.volume = 0;
+				startFadeIn = true;
+			}
+		}
+		if (startFadeIn == true) {
+			audioSource.volume += Time.deltaTime / fadeInTime;
+			if (audioSource.volume >= 1) {
+				audioSource.volume = 1;
+				startFadeIn = false;
+			}
+		}
+		if (stopMusic) {
+			audioSource.volume -= Time.deltaTime / stopFade;
+			if (audioSource.volume <= 0) {
+				audioSource.volume = 0;
+				stopMusic = false;
+			}
+		}
+	}
+
+	public void ChangeMusic(int songNumber, float fadeTime) {
+		startFadeIn = false;
+		stopMusic = false;
+		newSongNumber = songNumber;
+		if (fadeTime == 0) {
+			fadingTime = 0;
+			audioSource.clip = musicList[newSongNumber];
+			audioSource.volume = 1;
+			audioSource.Play();
+		} else if (fadeTime > 0) {
+			fadingTime = fadeTime;
+			fadeInTime = fadeTime;
+		}
+	}
+
+	public void StopMusic(float afadeTime) {
+		fadingTime = 0;
+		startFadeIn = false;
+		if (afadeTime == 0) {
+			audioSource.Stop();
+		}
+		else {
+			stopFade = afadeTime;
+			stopMusic = true;
+		}
+	}
+}
